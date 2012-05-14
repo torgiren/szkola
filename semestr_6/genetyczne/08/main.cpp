a7600cd84c8a5f7d347597ca67464c92d9508efb
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-05-14 18:31:59 +0200

    na uczelni, ale nie ma ga.h

--- semestr_6/genetyczne/08/main.cpp
+++ semestr_6/genetyczne/08/main.cpp
@@ -2,6 +2,10 @@
 #include <cmath>
 #include <ga/ga.h>
 using namespace std;
+  int popsize  = 10;
+  int ngen     = 1000;
+  float pmut   = 0.01;
+  float pcross = 0.6;
 double funkcja(double x, double y)
 {
 	const double a=1,b=2;
@@ -15,9 +19,26 @@ int main(int argc, char* argv[])
 	map.add(30,0,10);
 	GABin2DecGenome genome(map,objective);
 	GASimpleGA ga(genome);
+	ga.population(popsize);
+	ga.nGenerations(ngen);
+	ga.pMutation(pmut);
+	ga.pCrossover(pcross);
+
+	GANoScaling scaling;
+	ga.scaling(scaling);
+
+	ga.scoreFilename("zbieznosc.dat");
+	ga.scoreFrequency(10);
+	ga.flushFrequency(50);
+	go.evolve((unsigned)time(0));
+
+	GABin2DecGenome best_genome(map,objective);
+	best_benome=ga.statistics().bestIndividual();
+	cout<<"Najlepsze rozwiazanie: "<<best_genome.phenotype(0)<<"=>"<<objective(best_genome)<<endl;
+	cout<<"Najlepsze rozwiazanie2: "<<ga.population().best()<<":"<<ga.population().best().evaluate()<<":"<<ga.population().best().fitness()<<endl;
 	return 0;
 };
 float objective(GAGenome& gen)
 {
-	return funkcja(1,2);
+	return 1.0f/funkcja(1,2);
 };
