a56bcccd44a16e222c386ded34100d1b3da5a924
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-05-25 01:30:19 +0200

    template do plotowania pudla z wasami

--- semestr_6/genetyczne/08/plot_template.sh
+++ semestr_6/genetyczne/08/plot_template.sh
@@ -2,8 +2,9 @@
 # set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 500, 350 
 # set output 'candlesticks.5.png'
 set term jpeg
-set out "plot.jpg"
+set out "plot_template.jpg"
 set boxwidth 0.2 absolute
 set bars 4.0
  set style fill empty
-plot 'zbieznosc.dat' using 1:(-10):(-5):(0):(15):(10) with candlesticks
+plot 'data_0.01-0.7-50.1.dat' using 1:3:2:6:5 with candlesticks lt 3 lw 2 title 'Quartiles',      ''                 using 1:4:4:4:4 with candlesticks lt -1 lw 2
+
