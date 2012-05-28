ee189dcde2be9a6347c304ab239e82c1a67f4263
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-05-28 19:23:42 +0200

    znajdowanie trojkatow, bez obliczania pola

--- semestr_6/genetyczne/10/main.cpp
+++ semestr_6/genetyczne/10/main.cpp
@@ -3,44 +3,82 @@
 #include <ga/ga.h>
 #include <vector>
 using namespace std;
-const int popsize=10;
-const int ngen=1000;
+const int popsize=300;
+const int ngen=100;
 const float pmut=0.01;
-const float pcross=0.6;
+const float pcross=0.95;
+float objective(GAGenome &);
+void init(GAGenome& gen);
+bool PossibleTriangle(float,float,float);
+int tab[300];
 int main()
 {
 	srand(time(NULL));
 	ifstream plik;
 	plik.open("input.dat");
-	int tab[300];
-	int i,j;
+	int i;
 	for(i=0;i<300;i++)
 	{
 		plik>>tab[i];
 	};
 	plik.close();
-	vector<GA1DArrayGenome<int>*> pop;
-	for(i=0;i<popsize;i++)
+	GA1DArrayGenome<int> pop(300,objective);
+	pop.initializer(init);
+	pop.mutator(GA1DArrayGenome<int>::SwapMutator);
+	pop.crossover(GA1DArrayGenome<int>::PartialMatchCrossover);
+
+	GASimpleGA ga(pop);
+	ga.selector(GARankSelector());
+	ga.populationSize(popsize);
+	ga.nGenerations(ngen);
+	ga.pMutation(pmut);
+	ga.pCrossover(pcross);
+
+	ga.evolve(time(NULL));
+
+
+	cout << "Najlepsze rozwiazanie to F(x=";
+	cout << ga.population().best() << ")=";
+	cout << ga.population().best().evaluate() << " (";
+	cout << ga.population().best().fitness() << ")" << endl;
+
+
+
+};
+float objective(GAGenome& gen)
+{
+	int i;
+	GA1DArrayGenome<int>& genome=(GA1DArrayGenome<int>&) gen;
+	float wynik=0;
+	for(i=0;i<100;i++)
 	{
-		GA1DArrayGenome<int>* tmp=new GA1DArrayGenome<int>(300);
-		for(j=0;j<300;j++)
+		int a=genome[3*i];
+		int b=genome[3*i+1];
+		int c=genome[3*i+2];
+		if(PossibleTriangle(tab[a],tab[b],tab[c]))
 		{
-			tmp[j]=j;
-		};
-		pop.push_back(tmp);
-	};
-	for(i=0;i<popsize;i++)
-	{
-		for(j=0;j<50;j++)
-		{
-			pop[i]->swap(rand()%300,rand()%300);
+//			printf("Mozliwy z bokow: %d %d %d\n",tab[a],tab[b],tab[c]);
+			wynik+=100;
 		};
 	};
-
-
-
-	for(i=0;i<popsize;i++)
+	return wynik;
+};
+void init(GAGenome& gen)
+{
+	GA1DArrayGenome<int>& genome=(GA1DArrayGenome<int>&) gen;
+	int i;
+	for(i=0;i<genome.size();i++)
 	{
-		delete pop[i];
+		genome[i]=i;
 	};
+	for(i=0;i<50;i++)
+	{
+		genome.swap(rand()%genome.size(),rand()%genome.size());
+	};
+};
+bool PossibleTriangle(float a,float b,float c)
+{
+	bool possible=false;
+	if((a+b>c)&&(a+c>b)&&(b+c>a)) possible=true;
+	return possible;
 };
