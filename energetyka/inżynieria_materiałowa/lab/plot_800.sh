e10b2d11b0485dbdde4e08f3466eeac2838999b1
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-10-19 13:32:10 +0200

    skrypty do plotow

---
+++ energetyka/inżynieria_materiałowa/lab/plot_800.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/gnuplot
+set term jpeg
+set out "800.jpg"
+set xlabel "czas"
+set ylabel "masa"
+set logscale x
+set logscale y
+plot "dane.txt" using 1:3 w l smooth bezier,\
+	"dane.txt" using 1:3 w p
+#	"750.dat" using 1:2 w l smooth csplines,\
