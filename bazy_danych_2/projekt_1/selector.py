5536e83e342c1937c78af5b00c01940980dc1eab
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-11-22 16:23:50 +0100

    wybieranie bazy, tabeli i lista kolumn

--- bazy_danych_2/projekt_1/selector.py
+++ bazy_danych_2/projekt_1/selector.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 import bazowiec
 import interface
+sel_baza=None
+sel_table=None
 def mainWindow(face,actBaza=None,actTabela=None):
-	pass
+	title=face.newwin("title",3,70,1,1)
+	face.addstr("title","Baza: %s, tabela: %s"%(actBaza,actTabela))
+
 def selectDatabase(baza,face):
 	OPTS=baza.listDatabases()
-	return face.menu("bazy","Bazy:",15,30,1,1,2,1,OPTS)
-def main():
-	baza= bazowiec.Bazowiec()
-	baza.connect()
-	face=interface.Interface()
-	sel_baza=selectDatabase(baza,face)
-	return  sel_baza
-print main()
+	sel=face.menu("bazy","Bazy:",15,30,4,1,2,1,OPTS)
+	baza.setDatabase(sel)
+	return sel
+def selectTable(baza,face):
+	OPTS=baza.listTables()
+	sel=face.menu("tabele","Tabele",15,30,4,1,2,1,OPTS)
+	return sel
+def selectColumn(baza,face,table):
+	OPTS=baza.listColumns(table)
+	sel=face.menu("columny","Columny:",15,30,4,1,2,1,("%s - %s"%(row[0],row[1]) for row in OPTS))
+	return sel
+baza= bazowiec.Bazowiec()
+baza.connect()
+face=interface.Interface()
+mainWindow(face,sel_baza,sel_table)
+sel_baza=selectDatabase(baza,face)
+mainWindow(face,sel_baza,sel_table)
+sel_table=selectTable(baza,face)
+mainWindow(face,sel_baza,sel_table)
+selectColumn(baza,face,sel_table)
+face.__del__()
+print sel_baza
