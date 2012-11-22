b0e99b9f9357b002ba35ee57009be9558444a378
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-11-22 13:52:58 +0100

    znaczki przesuwanego menu

--- bazy_danych_2/projekt_1/interface.py
+++ bazy_danych_2/projekt_1/interface.py
@@ -9,6 +9,7 @@ class Interface:
 		curses.curs_set(0)
 		curses.start_color()
 		curses.init_pair(1,curses.COLOR_WHITE,curses.COLOR_BLUE)
+		curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
 		self.windows={}
 	def __del__(self):
 		curses.endwin()
--- bazy_danych_2/projekt_1/menu.py
+++ bazy_danych_2/projekt_1/menu.py
@@ -12,12 +12,16 @@ class Menu(Window):
 		self.itsOpts.append(opt)
 	def redraw(self):
 #		self.itsWindow.clear()
-		for i,o in enumerate(self.itsOpts[self.ViewStart:self.ViewStart+self.itsSizeY]):
+		for i,o in enumerate(self.itsOpts[self.ViewStart:self.ViewStart+self.itsSizeY-2]):
 			if self.itsAct==i+self.ViewStart:
 				style=curses.color_pair(1)|curses.A_BOLD
 			else:
 				style=curses.color_pair(0)
-			self.itsWindow.addstr(i,0,o,style)
+			self.itsWindow.addstr(1+i,0,o,style)
+			if self.ViewStart>0:
+				self.itsWindow.addch(0,0,'^',curses.color_pair(2))
+			if self.ViewStart+self.itsSizeY-2<len(self.itsOpts):
+				self.itsWindow.addch(self.itsSizeY-1,0,'v',curses.color_pair(2))
 	def Up(self):
 		self.itsAct-=1
 		self.CheckAct()
@@ -34,8 +38,8 @@ class Menu(Window):
 
 		if self.itsAct<self.ViewStart:
 			self.ViewStart=self.itsAct
-		if self.itsAct>self.ViewStart+self.itsSizeY-1:
-			self.ViewStart=self.itsAct-self.itsSizeY+1
+		if self.itsAct>self.ViewStart+self.itsSizeY-3:
+			self.ViewStart=self.itsAct-self.itsSizeY+3
 	def driver(self,key):
 		if key==curses.KEY_UP:
 			self.Up()	
