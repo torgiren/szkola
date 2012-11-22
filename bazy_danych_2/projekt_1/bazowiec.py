e32eb0af159d9fb120059b3c6ed7cfa64bd8a273
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-11-22 02:26:20 +0100

    Odczytywanie kolumn

--- bazy_danych_2/projekt_1/bazowiec.py
+++ bazy_danych_2/projekt_1/bazowiec.py
@@ -3,8 +3,11 @@ import pymssql
 conn=pymssql.connect(host='localhost:14330',user='marcin',password='Project123',database='AdventureWorks',as_dict=True)
 cur=conn.cursor()
 #cur.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
-cur.execute("exec sp_databases")
+#cur.execute("exec sp_databases")
+cur.execute("select * from information_schema.columns where table_name='Contact'")
 result=cur.fetchall()
 for row in result:
-	print row[0]
+#row[3] name
+#row[7] type
+	print row[3]
 conn.close()
