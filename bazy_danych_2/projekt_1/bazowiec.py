fefdc9af98e312fbabce42493419e3ad430086e9
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-11-22 02:15:04 +0100

    Lepsze zbieranie listy baz

--- bazy_danych_2/projekt_1/bazowiec.py
+++ bazy_danych_2/projekt_1/bazowiec.py
@@ -3,8 +3,8 @@ import pymssql
 conn=pymssql.connect(host='localhost:14330',user='marcin',password='Project123',database='AdventureWorks',as_dict=True)
 cur=conn.cursor()
 #cur.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
-cur.execute("select * from master..sysdatabases")
+cur.execute("exec sp_databases")
 result=cur.fetchall()
 for row in result:
-	print row['name']
+	print row[0]
 conn.close()
