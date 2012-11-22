b673c9844cb8ee8de73e5c78add91c7c5e1b9a6e
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-11-22 02:53:27 +0100

    refactoring

--- bazy_danych_2/projekt_1/bazowiec.py
+++ bazy_danych_2/projekt_1/bazowiec.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 import pymssql
-conn=pymssql.connect(host='localhost:14330',user='marcin',password='Project123',database='AdventureWorks',as_dict=True)
-cur=conn.cursor()
-#cur.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
-#cur.execute("exec sp_databases")
-cur.execute("select * from information_schema.columns where table_name='Contact'")
-result=cur.fetchall()
-for row in result:
+def connect():
+	return pymssql.connect(host='localhost:14330',user='marcin',password='Project123',database='AdventureWorks',as_dict=True)
+def setDatabase(conn,baza):
+	conn.cursor().execute("use %s"%baza)
+def listDatabases(conn):
+	cur=conn.cursor()
+	cur.execute("exec sp_databases")
+	return [row[0] for row in cur.fetchall()]
+def listTables(conn):
+	cur=conn.cursor()
+	cur.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
+	return ["%s.%s"%(row[1],row[2]) for row in cur.fetchall()]
+def listColumns(conn, table):
+	table=table.split('.')
+	cur=conn.cursor()
+	cur.execute("select * from information_schema.columns where table_schema='%s' and table_name='%s'"%(table[0],table[1]))
+	return [ (row[3],row[7]) for row in cur.fetchall()]
 #row[3] name
 #row[7] type
-	print row[3]
+conn=connect()
+lista=listColumns(conn,'Person.Contact')
+for i in lista:
+	print i
 conn.close()
