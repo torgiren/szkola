5b063238ef82383d0b820256d393df7139221d8d
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-11-22 02:13:47 +0100

    Pobieranie listy baz

--- bazy_danych_2/projekt_1/bazowiec.py
+++ bazy_danych_2/projekt_1/bazowiec.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import pymssql
-conn=pymssql.connect(host='localhost:14330',user='marcin',password='Project123',database='AdventureWorks')
+conn=pymssql.connect(host='localhost:14330',user='marcin',password='Project123',database='AdventureWorks',as_dict=True)
 cur=conn.cursor()
-cur.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
+#cur.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
+cur.execute("select * from master..sysdatabases")
 result=cur.fetchall()
 for row in result:
-	print row
+	print row['name']
 conn.close()
