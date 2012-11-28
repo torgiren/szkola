f421d0c76d1377a5a8e8d03bb889ca47c4fc44dc
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-11-28 17:06:49 +0100

    komentarze

--- bazy_danych_2/projekt_1/interface.py
+++ bazy_danych_2/projekt_1/interface.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import curses
 from window import Window
 from menu import Menu
 class Interface:
 	def __init__(self):
+		"""Inicjujemy interface curses"""
 		self.stdscr=curses.initscr()
 		self.stdscr.keypad(1)
 #		curses.noecho()
@@ -13,14 +16,18 @@ class Interface:
 		curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
 		self.windows={}
 	def __del__(self):
+		"""Kończymy działanie curses"""
 		curses.endwin()
 	def newwin(self,name,sizeY,sizeX,offsetY,offsetX, border=False):
+		"""Tworzymy nowe okno o zadanych parametrach"""
 		self.windows[name]=Window(sizeY,sizeX,offsetY,offsetX,border)
 		return self.windows[name]
 	def newmenu(self,name,sizeY,sizeX,offsetY,offsetX):
+		"""Tworzymy nowe menu"""
 		self.windows[name]=Menu(sizeY,sizeX,offsetY,offsetX,False)
 		return self.windows[name]
 	def menu(self,name,title,sizeY,sizeX,offsetY,offsetX,menuOffsetY,menuOffsetX,options,looped=False):
+		"""Tworzymy nowe menu"""
 		self.refresh()
 		self.windows[name]=Window(sizeY,sizeX,offsetY, offsetX,True)
 		self.windows[name].addstr(title)
@@ -34,16 +41,21 @@ class Interface:
 		men.redraw()
 		return men.run(self.stdscr)
 	def addstr(self,name,string):
+		"""Dodanie napisu do okna"""
 		self.windows[name].addstr(string)
 	def refresh(self):
+		"""Odświeżenie okna"""
 		self.stdscr.refresh()
 		for (i,k) in self.windows.items():
 			k.refresh()
 	def getch(self):
+		"""Pobranie znaku z okna"""
 		return self.stdscr.getch()
 	def getstr(self,x,y):
+		"""Pobranie stringa z danego okna"""
 		return self.stdscr.getstr(x,y)
 	def clear(self):
+		"""Wyczyszczenie okna"""
 		self.stdscr.clear()
 		for (i,k) in self.windows.items():
 			k.clear()
--- bazy_danych_2/projekt_1/selector.py
+++ bazy_danych_2/projekt_1/selector.py
@@ -7,53 +7,69 @@ sel_table=None
 wheres=[]
 selects=[]
 def makeQuery():
+	"""Zbudowanie zapytania używając wybranych column i kryteriów"""
 	cmd="select "
 	for i,s in enumerate(selects):
 		if i!=0:
 			cmd+=","
 		cmd+=s
-	cmd+="\nfrom %s where\n"%sel_table
-	for s in wheres:
-		cmd+=s
+	cmd+="\nfrom %s \n"%sel_table
+	if(wheres):
+		cmd+=" where "
+		for s in wheres:
+			cmd+=s
 	return cmd	
 def mainWindow(face):
+	"""Tworzenie głównego okna programu"""
 	global selects
 	title=face.newwin("title",8,70,1,1)
 	face.addstr("title","Baza: %s, tabela: %s\n"%(sel_baza,sel_table))
 	cmd=makeQuery()
 	face.addstr("title",cmd)
 def selectDatabase(baza,face):
+	"""Tworzenie menu do wybory bazy danych"""
 	OPTS=baza.listDatabases()
 	sel=face.menu("bazy","Bazy:",15,40,9,1,2,1,OPTS)
 	baza.setDatabase(sel)
 	return sel
 
 def selectTable(baza,face):
+	"""Tworzenie menu do wyboru tablicy w aktualnej bazie banych"""
 	OPTS=baza.listTables()
 	sel=face.menu("tabele","Tabele",15,40,9,1,2,1,OPTS)
 	return sel
 def selectColumn(baza,face,table,title):
+	"""Tworzenie menu do wyboru kolumny w aktualnej tabeli"""
 	OPTS=baza.listColumns(table)
 	sel=face.menu("columny",title,15,40,9,1,2,1,("%s:%s"%(row[0],row[1]) for row in OPTS),True)
 	return sel
 
 def selectType(baza,face):
+	"""Tworzenie menu do wyboru typu kryterium"""
 	OPTS=['=','!=','>','<','like']
 	sel=face.menu("typ","Typ warunku",11,20,12,4,2,1,OPTS,False)
 	return sel
 
 def selectComp(baza,face):
+	"""Tworzenie menu do wyboru typu łączenia kryteriów"""
 	OPTS=['and','or']
 	sel=face.menu("typ","Kombinacja",11,20,12,4,2,1,OPTS,False)
 	return sel
-	
+
+#łączenie z bazą danych
 baza= bazowiec.Bazowiec()
 baza.connect()
+#inicjacja interfejsu
 face=interface.Interface()
+#tworzenie okna głównego
 mainWindow(face)
+#wybór bazy danych
 sel_baza=selectDatabase(baza,face)
 mainWindow(face)
+#wybór tabeli
 sel_table=selectTable(baza,face)
+
+#wybór kolumn do wyselekcjonowania
 while 1:
 	mainWindow(face)
 	sel=selectColumn(baza,face,sel_table,'Co wyselekcjonowac')
@@ -62,6 +78,8 @@ while 1:
 	else:
 		global selects
 		selects.append(sel.split(':')[0])
+
+#wybór kryteriów do wyselekcjonowania
 while 1:
 	mainWindow(face)
 	sel=selectColumn(baza,face,sel_table,'Kryteria')
@@ -77,7 +95,14 @@ while 1:
 		val=face.getstr(15,6)
 		global wheres
 		wheres.append("%s %s %s '%s' "%(comb,sel.split(':')[0],type,val))
+#wyłączenie interfejsu curses
 face.__del__()
+
+#wypisanie wygenerowanego zapytania
+print "Wynik:\n"
+print makeQuery()
+
+#wypisanie wyniku otrzymanego zapytania
 for i in baza.query(makeQuery()):
 	print i
 
