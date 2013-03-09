1ef677a0fd5c712aa5fab1d35ac2086568e67423
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2013-03-09 22:36:50 +0100

    Prawdopodobnie skonczony program

--- semestr_8/analiza_obrazu/02/image_anal.py
+++ semestr_8/analiza_obrazu/02/image_anal.py
@@ -2,6 +2,7 @@
 """Moduł zawierający klasy używane do przetwarzania obrazu"""
 import numpy as np
 from scipy import misc
+import itertools
 
 
 class NoImageError(Exception):
@@ -11,6 +12,11 @@ class NoImageError(Exception):
 
 class NoSuchMethodError(Exception):
     """Wyjątek sygnalizujący podanie złej metody operacji"""
+    pass
+
+
+class FilterSizeError(Exception):
+    """Wyjątek sygnalizujący błędny format filtru"""
     pass
 
 
@@ -84,7 +90,7 @@ class ImageAnal:
             self.__scale_up(factor)
 
     @image_loaded
-    def progrowanie(self, method="global", otoczenie=5, odbieg=15):
+    def progowanie(self, method="global", otoczenie=5, odchylenie=15):
         """Przeprowadza progowanie obrazka.
         metody:
         global - progowanie globalne
@@ -93,15 +99,52 @@ class ImageAnal:
 
         parametry:
         otoczenie = rozmiar otoczenia pixela
-        odbieg - stopień odbiegania od średniej"""
+        odchylenie - stopień ochylenia od średniej"""
+        self.__grayscale1()
         if method == "global":
             self.__progowanie_globalne()
         elif method == "local":
             self.__progowanie_lokalne(otoczenie=otoczenie)
-
+        elif method == "mixed":
+            self.__progowanie_mieszane(otoczenie=otoczenie, odchylenie=odchylenie)
+        self.__clear_alpha()
+    @image_loaded
+    def splot(self, filter):
+        filter = np.array(filter, dtype=np.int8)
+        if filter.shape != (3,3):
+            raise(FilterSizeError)
+        data = self.__image
+        new = self.__expand(data, 1)
+        new = np.array(new, dtype=np.int32)
+#        new = np.array(new, dtype=np.uint8)
+#        print (filter[0,0] * new[:-2,:-2])[160,130]
+#        print (filter[0,1] * new[:-2,1:-1])[160,130]
+#        print (filter[0,2] * new[:-2,2:])[160,130]
+#        print (filter[1,0] * new[1:-1,:-2])[160,130]
+#        print (filter[1,1] * new[1:-1,1:-1])[160,130]
+#        print (filter[1,2] * new[1:-1,2:])[160,130]
+#        print (filter[2,0] * new[2:,:-2])[160,130]
+#        print (filter[2,1] * new[2:,1:-1])[160,130]
+#        print (filter[2,2] * new[2:,2:])[160,130]
+        new = (filter[0,0] * new[:-2,:-2] + filter[0,1] * new[:-2,1:-1] +\
+               filter[0,2] * new[:-2,2:] + filter[1,0] * new[1:-1,:-2] +\
+               filter[1,1] * new[1:-1,1:-1] + filter[1,2] * new[1:-1,2:] +\
+               filter[2,0] * new[2:,:-2] + filter[2,1] * new[2:,1:-1] +\
+               filter[2,2] * new[2:,2:])
+        new = new / (filter.sum())
+        new -= 255
+        new = new * (new<0)
+        new += 255
+        new = new * (new>0)
+        data = np.array(new, dtype=np.uint8)
+        self.__image = data
+#        self.normalize()
+        
+    
     @image_loaded
     def save(self, path):
         """Zapisuje obraz do pliku"""
+        self.__clear_alpha()
         misc.imsave(path, self.__image)
 
     def __grayscale1(self):
@@ -110,9 +153,11 @@ class ImageAnal:
 #        data[:,:] = 3 * (data[:,:].mean())
 #        x = [4 * (int(x.mean()),) for x in data]
         size = data.shape
-        tmp = data.reshape(size[0] * size[1], size[2])
-        tmp = np.array([size[2] * [z[:3].mean(), ] for z in tmp])
-        data = tmp.reshape((size[0], size[1], size[2]))
+        new = np.array(data, dtype=np.uint32)
+        new[:,:,0] += data[:,:,1]
+        new[:,:,0] += data[:,:,2]
+        new[:,:,0] /=3
+        data[:,:,1] = data[:,:,2] = data[:,:,0] = new[:,:,0]
 
         self.__image = data
 
@@ -131,18 +176,57 @@ class ImageAnal:
                 new[x * factor:(x + 1) * factor, y * factor:(y + 1) * factor, :] = data[x, y, :]
         self.__image = new
 
-    def __progowanie_globalne(self, otoczenie, odbieg):
-        self.__grayscale1()
+    def __progowanie_globalne(self, *args, **kwargs):
         data = self.__image
-        mean = data[:, :, 0].mean()
+        mean = self.__prog_globalny()
+#        mean = data[:, :, 0].mean()
         data = (data > mean) * 255.
         self.__image = data
 
-    def __progowanie_lokalne(self, otoczenie):
-        self.__grayscale1()
+    def __progowanie_lokalne(self, otoczenie=5, *argx, **kwargs):
         data = self.__image
-        new = data.copy()
+        prog = self.__prog_lokalny( otoczenie)
+        data = (data > prog) * 255
+        self.__image = data
 
+    def __progowanie_mieszane(self, otoczenie, odchylenie):
+        print "progowanie mieszne"
+        data = self.__image
+        prog = self.__prog_mieszany(otoczenie, odchylenie)
+        data = (data > prog) * 255
+        self.__image = data
+
+    def __prog_globalny(self):
+        data = self.__image
+        return data[:,:,0].mean()
+
+    def __prog_lokalny(self,otoczenie):
+        data = self.__image 
+        new = self.__expand(data, otoczenie)
+        prog = np.zeros(data.shape)
+#        for x in xrange(otoczenie, new.shape[0] - otoczenie):
+#            for y in xrange(otoczenie, new.shape[1] - otoczenie):
+#                prog[x - otoczenie, y - otoczenie] = new[x - otoczenie: x + otoczenie, y - otoczenie:y + otoczenie, :3].mean()
+        for d in itertools.product(np.arange(0,2*otoczenie+1), repeat=2):
+            prog[:,:] += new[d[0]:new.shape[0]-2*otoczenie+d[0],
+                             d[1]:new.shape[1]-2*otoczenie+d[1]]
+
+        prog /= (2*otoczenie+1)**2
+#        print prog
+        return prog
+
+    def __prog_mieszany(self, otoczenie, odchylenie):
+        glob = self.__prog_globalny()
+        prog = self.__prog_lokalny(otoczenie)
+        prog -= (glob + odchylenie)
+        prog = prog * (prog>0)
+        prog -= 2 * odchylenie
+        prog = prog * (prog<0)
+        prog += (glob + odchylenie)
+        return prog
+
+    def __expand(self, src, otoczenie):
+        data = src.copy()
         left = data[:, 0, :]
         right = data[:, -1, :]
         for i in xrange(otoczenie - 1):
@@ -150,24 +234,18 @@ class ImageAnal:
             right = np.column_stack((right, data[:, -1, :]))
         left = left.reshape((data.shape[0], -1, data.shape[2]))
         right = right.reshape((data.shape[0], -1, data.shape[2]))
-        new = np.column_stack((left, new, right))
-        top = new[0, :, :]
-        bottom = new[-1, :, :]
+        data = np.column_stack((left, data, right))
+        top = data[0, :, :]
+        bottom = data[-1, :, :]
         for i in xrange(otoczenie - 1):
-            top = np.column_stack((top, new[0, :, :]))
-            bottom = np.column_stack((bottom, new[-1, :, :]))
-        top = top.reshape((-1, new.shape[1], new.shape[2]))
-        bottom = bottom.reshape((-1, new.shape[1], new.shape[2]))
-        new = np.vstack((top, new, bottom))
-        for x in xrange(otoczenie, new.shape[0] - otoczenie):
-            for y in xrange(otoczenie, new.shape[1] - otoczenie):
-                data[x - otoczenie, y - otoczenie] = (new[x, y] > new[x - otoczenie: x + otoczenie, y - otoczenie:y + otoczenie, :3].mean()) * 255
-        self.__image = data
-        self.__clear_alpha()
-
-    def __progowanie_mieszane(self, otoczenie, odbieg):
-        pass
+            top = np.column_stack((top, data[0, :, :]))
+            bottom = np.column_stack((bottom, data[-1, :, :]))
+        top = top.reshape((-1, data.shape[1], data.shape[2]))
+        bottom = bottom.reshape((-1, data.shape[1], data.shape[2]))
+        data = np.vstack((top, data, bottom))
+        return data
 
     def __clear_alpha(self):
+        print "clear alpha"
         if self.__image.shape[2] == 4:
             self.__image[:, :, 3] = 255
