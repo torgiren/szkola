4eef70878978f64151da90f188aaa472094462a9
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2013-04-11 15:40:20 +0200

    wyniki koncowe

--- semestr_8/modelowanie/04-05/lab04.py
+++ semestr_8/modelowanie/04-05/lab04.py
@@ -14,10 +14,6 @@ xp = 90.0 / dx
 m = 1
 Ca = U * dt / dx
 Cd = D * dt / dx ** 2
-C1 = Cd * (1.0 - Ca) - Ca / 6.0 * (Ca ** 2 - 3.0 * Ca + 2.0)
-C2 = Cd * (2.0 - 3.0 * Ca) - Ca / 2.0 * (Ca ** 2 - 2.0 * Ca - 1.0)
-C3 = Cd * (1.0 - 3.0 * Ca) - Ca / 2.0 * (Ca ** 2 - Ca - 2.0)
-C4 = Cd * Ca + Ca / 6.0 * (Ca ** 2 - 1.0)
 
 
 def f1():
@@ -25,6 +21,10 @@ def f1():
 
 
 def method1():
+    C1 = Cd * (1.0 - Ca) - Ca / 6.0 * (Ca ** 2 - 3.0 * Ca + 2.0)
+    C2 = Cd * (2.0 - 3.0 * Ca) - Ca / 2.0 * (Ca ** 2 - 2.0 * Ca - 1.0)
+    C3 = Cd * (1.0 - 3.0 * Ca) - Ca / 2.0 * (Ca ** 2 - Ca - 2.0)
+    C4 = Cd * Ca + Ca / 6.0 * (Ca ** 2 - 1.0)
     vec = np.zeros(L / dx)
     vec[x0] = f1()
     hist = []
@@ -49,22 +49,15 @@ def method2():
     vec = np.zeros(L / dx)
     vec[x0] = f1()
     hist = []
-    C1 = 1.0 + Cd
-    C2 = -Cd / 2.0 + Ca / 4.0
-    C3 = -Cd / 2.0 - Ca / 4.0
-    C4 = 1.0 - Cd
-    C5 = Cd / 2.0 - Ca / 4.0
-    C6 = Cd / 2.0 + Ca / 4.0
-
     s = int(L / dx)
-    AA = np.diag([C1] * s)
-    AA[0:-1, 1:] += np.diag([C2] * (s - 1))
-    AA[1:, 0:-1] += np.diag([C3] * (s - 1))
+    AA = np.diag([1.0 + Cd] * s)
+    AA[0:-1, 1:] += np.diag([-Cd / 2.0 + Ca / 4.0] * (s - 1))
+    AA[1:, 0:-1] += np.diag([-Cd / 2.0 - Ca / 4.0] * (s - 1))
     AA = np.linalg.inv(AA)
 
-    BB = np.diag([C4] * s)
-    BB[0:-1, 1:] += np.diag([C5] * (s - 1))
-    BB[1:, 0:-1] += np.diag([C6] * (s - 1))
+    BB = np.diag([1.0 - Cd] * s)
+    BB[0:-1, 1:] += np.diag([Cd / 2.0 - Ca / 4.0] * (s - 1))
+    BB[1:, 0:-1] += np.diag([Cd / 2.0 + Ca / 4.0] * (s - 1))
 
     CC = np.dot(AA, BB)
     vec[x0] = f1()
@@ -85,4 +78,5 @@ def method2():
     plt.plot(xrange(len(hist)), hist)
     plt.savefig("plots2/hist.png")
 
+method1()
 method2()
