a3a83b54ffa8c15c5a9015f88aba5bfe8b772948
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2013-04-13 17:04:00 +0200

    Maska

--- semestr_8/analiza_obrazu/06/lab06.py
+++ semestr_8/analiza_obrazu/06/lab06.py
@@ -2,7 +2,12 @@
 #*-* coding: utf-8 *-*
 from image_anal import ImageAnal
 img = ImageAnal()
-img.open("12-t1.bmp")
-#img.open("test.png")
-img.KKM()
-img.save('kkm.png')
+files = ['12-t1', '12-t2', '12-t3', '12-t4']
+for f in files:
+    img.open(f+".bmp")
+    img.KKM()
+    img.save(f+'_kkm.png')
+
+    img.open(f+".bmp")
+    img.maska()
+    img.save(f+'_maska.png')
