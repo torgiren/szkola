447296566919b20905af62a9aad666b7ff8681d5
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2013-05-03 16:15:56 +0200

    wstepny projekt

---
+++ semestr_8/eksploracja_danych/projekt/FabrykowskiMarcin.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#*-* coding: utf8 *-*
+print "Ładownie modułów: ",
+import sys
+try:
+	import scipy
+except(ImportError):
+	print "Warning: Brak modulu scipy"
+try:
+	import Orange
+except(ImportError):
+	print "Error: Brak modułu Orange"
+	sys.exit(-1)
+try:
+	import pygraphviz as pgv
+except(ImportError):
+	print "Error: Brak modułu pygraphviz"
+	sys.exit(-1)
+print "OK"
+
+print "Wczytywanie danych: ",
+sys.stdout.flush()
+data = Orange.data.Table("winequality-white.csv")
+print "OK"
+print "Tworzenie drzewa: ",
+sys.stdout.flush()
+tree = Orange.classification.tree.TreeLearner(data, m_pruning=2, max_depth=10)
+print "OK"
+print "Generowanie pliku .dot: ", 
+sys.stdout.flush()
+tree.dot(file_name="FabrykowskiMarcin.dot", leaf_shape='box')
+print "OK"
+print "Generowanie wykresu: ",
+sys.stdout.flush()
+G = pgv.AGraph("FabrykowskiMarcin.dot")
+G.layout(prog='dot')
+G.draw(path="FabrykowskiMarcin.png", prog='dot')
+print "OK"
