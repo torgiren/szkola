5fc79cf7d991d8b334434dfc8c3fd5edfbca1372
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2013-06-13 18:14:32 +0200

    kolejne etapy :P

---
+++ semestr_8/analiza_obrazu/projekt/rozpoznaj.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#!/usr/bin/env python
+#*-* coding: utf8 *-*
+import glob
+from scipy import misc
+from image_anal import ImageAnal
+img = ImageAnal()
+print "Wczytuje obraz"
+img.open("test2.bmp")
+print "Zaczynam segmentacje"
+img.segment1('wynik')
+
+dane = glob.glob("dana/*/*")
+
+pliki = glob.glob('wynik/*')
+pliki.sort()
+tekst = []
+i = 0
+for p in pliki:
+    wyniki = []
+    test = misc.imread(p)
+    for f in dane:
+        d = misc.imread(f)
+        wyniki.append([abs(test - d).sum(), f])
+    wyniki.sort()
+
+    znak = wyniki[0][1].split('/')[1].decode('utf-8')[4]
+    tekst.append(znak)
+    i+=1
+    print str(i)+"/"+str(len(pliki))
+
+print "".join(tekst)
